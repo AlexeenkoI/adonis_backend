a4ca3e2131310b26c639f3f4d688e84d778f42a6
Igor Alekseenko <fastedx@gmail.com>
2018-12-04 22:30:21 +0300

    initial commit

---
+++ app/Models/Contracts.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Contracts extends Model {
+    static get createdAtColumn() {
+        return null;
+    }
+
+    static get updatedAtColumn() {
+        return null;
+    }
+
+    users() {
+        return this
+        .belongsToMany('App/Models/User')    
+        .pivotTable('performes')
+    }
+
+}
+
+module.exports = Contracts
---
+++ start/routes.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+const Database = use('Database');
+const Contracts = use('App/Models/Contracts');
+const Users = use('App/Models/User');
+
+Route.on('/').render('welcome')
+Route.get('/testquery', async ()=>{
+
+  //const contractsData = await Contracts.query().where('id',3).fetch();
+  //const users = await contractsData.users().fetch();
+    const contracts = await Contracts.find(3);
+    const usr = await Users.find(3)
+    const users = await contracts.users().fetch();
+    const test = await contracts.users().wherePivot('contract_id',contracts.id).fetch();
+    console.log(test);
+    let resp = {
+      data : contracts,
+      t: test
+    }
+    //console.log(contractsData);
+    //console.log(users);
+    //return await Database.table('contracts').first();
+    //return await Contracts.query().where('id',3).fetch();
+    //return await contractsData.users().fetch();
+    return resp;
+})
